6df82d51f8d414627db7f9d5cb0c4005ebc93b2e
Yuanchao Sun <yuanchao.sun@gmail.com>
2021-11-12 15:04:47 +0800

    Add the verification of messages (#2)
    * Refactoring
    
    * Add verification of messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,26 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 #[cfg(not(feature = "std"))]
 use core::result;
 
 use beefy_merkle_tree::{merkle_root, verify_proof, Hash, Keccak256, MerkleProof};
 use commitment::{Commitment, SignedCommitment};
+use header::Header;
 use mmr::MmrLeaf;
-use validator_set::{BeefyNextAuthoritySet, Public};
+use validator_set::{BeefyNextAuthoritySet, Public, ValidatorSetId};
 
 pub mod commitment;
-pub mod ecdsa;
+pub mod header;
 pub mod mmr;
 pub mod simplified_mmr;
-pub mod traits;
 pub mod validator_set;
-
-/// A marker struct for validator set merkle tree.
-#[derive(Debug)]
-pub struct ValidatorSetTree;
-
-/// A marker struct for the MMR.
-#[derive(Debug)]
-pub struct Mmr;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
 	/// [Commitment] can't be imported, cause it's signed by either past or future validator set.
-	// InvalidValidatorSetId { expected: ValidatorSetId, got: ValidatorSetId },
+	InvalidValidatorSetId { expected: ValidatorSetId, got: ValidatorSetId },
 	/// [Commitment] can't be imported, cause it's a set transition block and the proof is missing.
 	InvalidValidatorProof,
-	/// [Commitment] is not useful, cause it's made for an older block than we know of.
-	///
-	/// In practice it's okay for the light client to import such commitments (if the validator set
-	/// matches), but it doesn't provide any more value, since the payload is meant to be
-	/// cumulative.
-	/// It might be useful however, if we want to verify proofs that were generated against this
-	/// specific block number.
-	// OldBlock {
-	//     /// Best block currently known by the light client.
-	//     best_known: BlockNumber,
-	//     /// Block in the commitment.
-	//     got: BlockNumber,
-	// },
 	/// There are too many signatures in the commitment - more than validators.
 	InvalidNumberOfSignatures {
 		/// Number of validators in the set.
@@ -55,33 +36,22 @@ pub enum Error {
 	InvalidMmrProof,
 	///
 	InvalidSignature,
+	///
+	InvalidMessage,
+	///
+	InvalidRecoveryId,
+	///
+	WrongSignature,
+	///
+	InvalidMmrLeafProof,
+	///
+	DigestNotFound,
+	///
+	DigestNotMatch,
+	///
+	HeaderHashNotMatch,
 }
 
-// $ subkey inspect --scheme ecdsa //Alice
-// Secret Key URI `//Alice` is account:
-//   Secret seed:       0xcb6df9de1efca7a3998a8ead4e02159d5fa99c3e0d4fd6432667390bb4726854
-//   Public key (hex):  0x020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a1
-//   Public key (SS58): KW39r9CJjAVzmkf9zQ4YDb2hqfAVGdRqn53eRqyruqpxAP5YL
-//   Account ID:        0x01e552298e47454041ea31273b4b630c64c104e4514aa3643490b8aaca9cf8ed
-//   SS58 Address:      5C7C2Z5sWbytvHpuLTvzKunnnRwQxft1jiqrLD5rhucQ5S9X
-//
-// justifications.commitment.payload:  0x700a2fb21ba1ec2cdf72bb621846a4cc8628ed8e3ed5bb299f9e36406776f84a
-// justifications.commitment.blockNumber:  1369
-// justifications.commitment.validatorSetId:  0
-// justifications.commitment.hash:  0xd96c73e1a602b757fce3ff4509b57cda4f4989f854dac6753d8d3329049e93e8
-// justifications.signatures:  [0x3a481c251a7aa94b89e8160aa9073f74cc24570da13ec9f697a9a7c989943bed31b969b50c47675c11994fbdacb82707293976927922ec8c2124490e417af733]
-//
-// justifications.commitment.payload:  0x86b1679e44a6a525748707bd2d4d44700fc3a2dc9e152a8fd414b4e9d17b07b5
-// justifications.commitment.blockNumber:  1377
-// justifications.commitment.validatorSetId:  0
-// justifications.commitment.hash:  0x86262696075aeb15493d428a3353c3b60616c8380c0bb4966211a1b92a58634f
-// justifications.signatures:  [0xcc73f69bf58fbe1720c59a5f1a804a869012ed9f4e86637cfb85b0d126c86f916f1e7a3db98f2608e4968544fdb0bceb55b380d8c9fd9638ea0c420d47b6001f]
-//
-// justifications.commitment.payload:  0x3e793b247a32500619702d4d4c6ad63466cc06189904bccd428c17ac2e2b08e2
-// justifications.commitment.blockNumber:  1385
-// justifications.commitment.validatorSetId:  0
-// justifications.commitment.hash:  0x98d3357fa89e5c48f963d76846a526cbd6c40dbd67124b117065af872a1d3ef1
-// justifications.signatures:  [0xf994b9bf1410de7988806738ea7c046bbf964cb76d1e07104fd0d5a67925d76558b5cd27e5fc6d566940943f488692fd7bb54233d7e89d34d53dfa16a4b9fb63]
 #[derive(Debug, Default)]
 pub struct LightClient {
 	mmr_root: Hash,
@@ -134,11 +104,30 @@ impl LightClient {
 		Ok(())
 	}
 
-	pub fn verify_solochain_message(&self) -> Result<(), Error> {
+	pub fn verify_solochain_messages(
+		&self,
+		messages: &[u8],
+		header: Header,
+		leaf: &MmrLeaf,
+		proof: simplified_mmr::MerkleProof,
+	) -> Result<(), Error> {
+		let header_digest = header.get_other().ok_or(Error::DigestNotFound)?;
+
+		let messages_hash = Keccak256::hash(messages);
+		if messages_hash != &header_digest[..] {
+			return Err(Error::DigestNotFound);
+		}
+
+		let header_hash = header.hash();
+		if header_hash != leaf.parent_number_and_hash.1 {
+			return Err(Error::HeaderHashNotMatch);
+		}
+
+		self.verify_mmr_leaf(self.mmr_root, leaf, proof)?;
 		Ok(())
 	}
 
-	pub fn verify_parachain_message(&self) -> Result<(), Error> {
+	pub fn verify_parachain_messages(&self) -> Result<(), Error> {
 		Ok(())
 	}
 
@@ -146,14 +135,15 @@ impl LightClient {
 		let SignedCommitment { commitment, signatures } = signed_commitment;
 		let commitment_hash = commitment.hash();
 		println!("commitment_hash: {:?}", commitment_hash);
-		let msg = libsecp256k1::Message::parse_slice(&commitment_hash[..]).unwrap();
+		let msg = libsecp256k1::Message::parse_slice(&commitment_hash[..])
+			.or(Err(Error::InvalidMessage))?;
 		for signature in signatures.into_iter() {
 			if let Some(signature) = signature {
-				let sig = libsecp256k1::Signature::parse_standard_slice(&signature[..]).unwrap();
-				let res =
-					libsecp256k1::recover(&msg, &sig, &libsecp256k1::RecoveryId::parse(0).unwrap())
-						.unwrap();
-				println!("verify result: {:?}", res.serialize_compressed());
+				let sig = libsecp256k1::Signature::parse_standard_slice(&signature.0[..64])
+					.or(Err(Error::InvalidSignature))?;
+				let recovery_id = libsecp256k1::RecoveryId::parse(signature.0[64])
+					.or(Err(Error::InvalidRecoveryId))?;
+				libsecp256k1::recover(&msg, &sig, &recovery_id).or(Err(Error::WrongSignature))?;
 			}
 		}
 
@@ -162,12 +152,12 @@ impl LightClient {
 
 	fn verify_mmr_leaf(
 		&self,
-		root_hash: Hash,
+		root: Hash,
 		leaf: &MmrLeaf,
 		proof: simplified_mmr::MerkleProof,
 	) -> Result<(), Error> {
 		let leaf_hash = leaf.hash();
-		let result = simplified_mmr::verify_proof(root_hash, leaf_hash, proof);
+		let result = simplified_mmr::verify_proof(root, leaf_hash, proof);
 		if !result {
 			return Err(Error::InvalidMmrProof);
 		}
@@ -178,7 +168,7 @@ impl LightClient {
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use crate::{Commitment, SignedCommitment};
+	use crate::commitment::{Commitment, Signature, SignedCommitment};
 	use hex_literal::hex;
 
 	#[test]
@@ -194,12 +184,32 @@ mod tests {
 			block_number: 1369,
 			validator_set_id: 0,
 		};
-		let signed_commitment = SignedCommitment { commitment, signatures: vec![Some(hex!("3a481c251a7aa94b89e8160aa9073f74cc24570da13ec9f697a9a7c989943bed31b969b50c47675c11994fbdacb82707293976927922ec8c2124490e417af733").into())] };
+		let signed_commitment = SignedCommitment { commitment, signatures: vec![Some(Signature(hex!("3a481c251a7aa94b89e8160aa9073f74cc24570da13ec9f697a9a7c989943bed31b969b50c47675c11994fbdacb82707293976927922ec8c2124490e417af73300").into()))] };
 		let res = lc.verify_commitment(signed_commitment).unwrap();
 		println!("{:?}", res);
 
 		assert_eq!(2 + 2, 4);
 		// let pk = hex!("020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a1");
 		// let pk = libsecp256k1::PublicKey::parse_slice(&pk[..], None).unwrap();
+	}
+
+	#[test]
+	fn recover_works() {
+		let msg = libsecp256k1::Message::parse_slice(&hex!(
+			"14f213146a362c397545659ac7795926514696ad49565972d64964040394482c"
+		))
+		.unwrap();
+		let signature =  Signature(hex!("3a481c251a7aa94b89e8160aa9073f74cc24570da13ec9f697a9a7c989943bed31b969b50c47675c11994fbdacb82707293976927922ec8c2124490e417af73300").into());
+		let sig = libsecp256k1::Signature::parse_standard_slice(&signature.0[..64]).unwrap();
+		let public_key = libsecp256k1::recover(
+			&msg,
+			&sig,
+			&libsecp256k1::RecoveryId::parse(signature.0[64]).unwrap(),
+		)
+		.unwrap();
+		assert_eq!(
+			public_key.serialize_compressed(),
+			hex!("020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a1")
+		);
 	}
 }
--- src/mmr.rs
+++ src/mmr.rs
@@ -1,6 +1,9 @@
 use crate::BeefyNextAuthoritySet;
-use beefy_merkle_tree::{Hash, Keccak256};
+use beefy_merkle_tree::{verify_proof, Hash, Keccak256, Leaf, MerkleProof};
 use codec::Encode;
+
+#[cfg(not(feature = "std"))]
+use core::convert::Into;
 
 #[derive(Debug, Default, Encode)]
 pub struct MmrLeafVersion(u8);
@@ -41,4 +44,93 @@ impl MmrLeaf {
 	pub fn hash(&self) -> Hash {
 		Keccak256::hash(&self.encode())
 	}
+}
+
+impl<'a> From<MmrLeaf> for Leaf<'a> {
+	fn from(v: MmrLeaf) -> Self {
+		Leaf::Hash(v.hash())
+	}
+}
+
+/// A MMR proof data for one of the leaves.
+#[derive(Debug, Default, Encode)]
+pub struct MmrLeafProof {
+	/// The index of the leaf the proof is for.
+	pub leaf_index: u64,
+	/// Number of leaves in MMR, when the proof was generated.
+	pub leaf_count: u64,
+	/// Proof elements (hashes of siblings of inner nodes on the path to the leaf).
+	pub items: Vec<Hash>,
+}
+
+/// MMR nodes & size -related utilities.
+pub struct NodesUtils {
+	no_of_leaves: u64,
+}
+
+impl NodesUtils {
+	/// Create new instance of MMR nodes utilities for given number of leaves.
+	pub fn new(no_of_leaves: u64) -> Self {
+		Self { no_of_leaves }
+	}
+
+	/// Calculate number of peaks in the MMR.
+	pub fn number_of_peaks(&self) -> u64 {
+		self.number_of_leaves().count_ones() as u64
+	}
+
+	/// Return the number of leaves in the MMR.
+	pub fn number_of_leaves(&self) -> u64 {
+		self.no_of_leaves
+	}
+
+	/// Calculate the total size of MMR (number of nodes).
+	pub fn size(&self) -> u64 {
+		2 * self.no_of_leaves - self.number_of_peaks()
+	}
+
+	/// Calculate maximal depth of the MMR.
+	pub fn depth(&self) -> u32 {
+		if self.no_of_leaves == 0 {
+			return 0;
+		}
+
+		64 - self.no_of_leaves.next_power_of_two().leading_zeros()
+	}
+}
+pub fn leaf_index_to_pos(index: u64) -> u64 {
+	// mmr_size - H - 1, H is the height(intervals) of last peak
+	leaf_index_to_mmr_size(index) - (index + 1).trailing_zeros() as u64 - 1
+}
+
+pub fn leaf_index_to_mmr_size(index: u64) -> u64 {
+	// leaf index start with 0
+	let leaves_count = index + 1;
+
+	// the peak count(k) is actually the count of 1 in leaves count's binary representation
+	let peak_count = leaves_count.count_ones() as u64;
+
+	2 * leaves_count - peak_count
+}
+
+/// Stateless verification of the leaf proof.
+pub fn verify_leaf_proof(
+	root: Hash,
+	leaf: MmrLeaf,
+	proof: MmrLeafProof,
+) -> Result<bool, crate::Error> {
+	let size = NodesUtils::new(proof.leaf_count).size();
+	let leaf_position = leaf_index_to_pos(proof.leaf_index);
+	let p = MerkleProof {
+		root,
+		proof: proof.items,
+		number_of_leaves: size as usize,
+		leaf_index: leaf_position as usize,
+		leaf,
+	};
+
+	if !verify_proof::<Keccak256, _, _>(&root, p.proof, p.number_of_leaves, p.leaf_index, p.leaf) {
+		return Err(crate::Error::InvalidMmrProof);
+	}
+	Ok(true)
 }
