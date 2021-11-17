2270d3fce4759c7a0ec897ad3a1ace70cd1c1989
Yuanchao Sun <yuanchao.sun@gmail.com>
2021-11-17 12:39:22 +0800

    add alloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(not(feature = "std"))]
+extern crate alloc;
+
+#[cfg(not(feature = "std"))]
 use core::result;
 #[cfg(not(feature = "std"))]
 use core::convert::TryInto;
